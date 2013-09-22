a52f5dc33df0eee5981c0afd0b8477b1d462a3f8
Bryn Sadler <bryn.sadler@gmail.com>
2013-09-22 02:03:51 +0100

    enabled admin for models

---
+++ capman/albacore/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from albacore.models import Circuit, Provider
+
+admin.site.register(Circuit)
+admin.site.register(Provider)
--- capman/albacore/models.py
+++ capman/albacore/models.py
@@ -4,16 +4,22 @@ from django.db import models
 
 class Provider(models.Model):
   name = models.CharField(max_length=100)
+  def __unicode__(self):
+    return self.name
   
 class Switch(models.Model):
   name = models.CharField(max_length=50)
   model = models.CharField(max_length=50)
   num_ports = models.IntegerField()
+  def __unicode__(self):
+    return self.name
 
 class SwitchPort(models.Model):
   name = models.CharField(max_length=2)
   switch = models.ForeignKey(Switch, related_name='switch')
   speed = models.IntegerField()
+  def __unicode__(self):
+    return self.name
 
 class Circuit(models.Model):
   name = models.CharField(max_length=50)
@@ -24,10 +30,14 @@ class Circuit(models.Model):
   bearer = models.IntegerField()
   pbdemuxport = models.ForeignKey(SwitchPort, related_name='pbdmuxport')
   metroeport = models.ForeignKey(SwitchPort, related_name='metroeport') 
+  def __unicode__(self):
+    return self.name
   
 class Datacentre(models.Model):
   name = models.CharField(max_length=50)
   description = models.CharField(max_length=500)
+  def __unicode__(self):
+    return self.name
 
 class Interconnect(models.Model):
   name = models.CharField(max_length=50)
@@ -37,13 +47,19 @@ class Interconnect(models.Model):
   dc = models.ForeignKey(Datacentre, related_name='int_dc')
   is_open = models.BooleanField()
   capacity = models.IntegerField()
+  def __unicode__(self):
+    return self.name
 
 class Site(models.Model):
   name = models.CharField(max_length=100)
   operator = models.ForeignKey(Switch, related_name='op')
   primary = models.ForeignKey(Circuit, related_name='pri_cct')
   backup = models.ForeignKey(Circuit, related_name='sec_cct')
+  def __unicode__(self):
+    return self.name
 
 class Operator(models.Model):
   name = models.CharField(max_length=100)  
-
+  def __unicode__(self):
+    return self.name
+    
